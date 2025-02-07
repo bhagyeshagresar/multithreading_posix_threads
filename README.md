69efffd0d2163c0b628856909bfecc2b6ebc53b7
bhagyeshagresar <bhagyeshagresar@gmail.com>
2025-02-06 22:48:19 -0600

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Multithreading Challenges 
+
+## Challenge 1 : Classic Producer-Consumer problem
+A simple problem to demonstrate producer/consumer problem using the pthreads API.
+A Shared Buffer of specific size is shared between a single producer and a consumer
+Producer does n write operations and consumer thread keeps reading from the buffer
+The problem involves using mutex to lock critical section and counting semaphores to keep track of elements filled
--- producer_consumer.c
+++ producer_consumer.c
@@ -1,8 +1,8 @@
 
-//A simple problem to demonstrate producer/consumer problem using the pthreads API.
-//A Shared Buffer of specific size is shared between a single producer and a consumer
-//Producer does n write operations and consumer thread keeps reading from the buffer
-//The problem involves using mutex to lock critical section and counting semaphores to keep track of elements filled
+// A simple problem to demonstrate producer/consumer problem using the pthreads API.
+// A Shared Buffer of specific size is shared between a single producer and a consumer
+// Producer does n write operations and consumer thread keeps reading from the buffer
+// The problem involves using mutex to lock critical section and counting semaphores to keep track of elements filled
 
 
 #include <stdio.h>
